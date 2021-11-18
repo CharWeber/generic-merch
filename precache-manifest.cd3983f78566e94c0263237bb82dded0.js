ce637ce88adb444f7eddad4bf5a6e124b11be0bd
Charlie Weber <charlestweber@gmail.com>
2021-11-18 10:43:15 -0800

    Updates

---
+++ precache-manifest.cd3983f78566e94c0263237bb82dded0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d60bf8810049fd22fa62170724e3ae",
+    "url": "/generic-merch/index.html"
+  },
+  {
+    "revision": "30b26d19a18cfb511343",
+    "url": "/generic-merch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee630b94bcc78bdd0eb3",
+    "url": "/generic-merch/static/js/2.46c73f67.chunk.js"
+  },
+  {
+    "revision": "23c3a8b6f5b957b11d8a",
+    "url": "/generic-merch/static/js/3.7a985a30.chunk.js"
+  },
+  {
+    "revision": "30b26d19a18cfb511343",
+    "url": "/generic-merch/static/js/main.5cd6c0ce.chunk.js"
+  },
+  {
+    "revision": "8cc18e20d651d614dbbd",
+    "url": "/generic-merch/static/js/runtime-main.3bd0fa47.js"
+  }
+]);
